eb648ec3f2fa0717d43910868f877435054d9b77
Jonas <jdecorte6@gmail.com>
2022-10-13 15:44:20 +0200

    update

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   festivalName: 'IRFestival',
   apiBaseUrl: 'https://irfestivalapijdc.azurewebsites.net/api/',
+  connectionstringAppInsights:
+    'InstrumentationKey=7636e875-06ab-4c63-9cb1-b02f4b022084;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/',
 };
