f063ed3a6fd0a10fe18e14e9b50ba7270d0bf111
Jonas <jdecorte6@gmail.com>
2022-10-17 11:46:20 +0200

    frontend

--- Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
+++ Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Schedule } from '../api/models/schedule.model';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaderResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppSettings } from '../api/models/appsettings.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppSettingsApiService {
   private baseUrl = environment.apiBaseUrl + 'settings';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getSettings(): Observable<AppSettings> {
-    return this.httpClient.get<AppSettings>(this.baseUrl);
+    const headers = new HttpHeaders().set(
+      'Ocp-Apim-Subscription-Key',
+      'b8d07e37b1d044e28bffea3f2ee68a91'
+    );
+    return this.httpClient.get<AppSettings>(this.baseUrl, {
+      headers: headers,
+    });
   }
 }
