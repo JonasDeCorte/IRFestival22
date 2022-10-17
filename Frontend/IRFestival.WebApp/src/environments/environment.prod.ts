646e786fc2310f22a0821b626e824c2739853080
Jonas <jdecorte6@gmail.com>
2022-10-17 16:19:16 +0200

    added

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -4,4 +4,5 @@ export const environment = {
   apiBaseUrl: 'https://irfestivalapimjdc.azure-api.net/2022/v1/api/',
   connectionstringAppInsights:
     'InstrumentationKey=7636e875-06ab-4c63-9cb1-b02f4b022084;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/',
+  redirectUrl: 'http://localhost:4200',
 };
